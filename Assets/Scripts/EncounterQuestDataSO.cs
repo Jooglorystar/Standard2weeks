9270504d14e017a1243840fe0172b9de408644da
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-10-22 20:04:55 +0900

    Create MonsterQuestDataSO, EncounterQuestDataSO

---
+++ Assets/Scripts/EncounterQuestDataSO.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class EncounterQuestDataSO : QuestDataSO
+{
+    public int TargetMetCount;
+}
---
+++ Assets/Scripts/MonsterQuestDataSO.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class MonsterQuestDataSO : QuestDataSO
+{
+    public int TargetGoalCount;
+}
