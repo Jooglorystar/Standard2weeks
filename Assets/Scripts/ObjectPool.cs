29828c83c4d0118ac7462a5ef251d7f0d16adba5
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-10-22 19:27:44 +0900

    Create Monster prefab

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,25 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class ObjectPool : MonoBehaviour
 {
-    public GameObject prefab;
-    private List<GameObject> pool = new List<GameObject>();
-    public int poolSize = 300;
+    [System.Serializable]
+    public class Pool
+    {
+        public string name;
+        public GameObject prefab;
+        public int poolSize = 300;
+        public List<GameObject> ObjsInPool = new List<GameObject>();
+    }
+
+    public List<Pool> pools = new List<Pool>();
+
+    private Dictionary<string, Pool> poolDict = new Dictionary<string, Pool>();
 
     void Start()
     {
+        /*
         for (int i = 0; i < poolSize; i++)
         {
             GameObject obj = Instantiate(prefab, transform);
-            pool.Add(obj);
+            pools.Add(obj);
             obj.SetActive(false);
+        }
+        */
+        foreach (var pool in pools)
+        {
+            for (int i = 0; i < pool.poolSize; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab, transform);
+                pool.ObjsInPool.Add(obj);
+                obj.SetActive(false);
+                Debug.Log($"{pool.name} »ý¼º {i}");
+            }
         }
     }
 
-    public GameObject Get()
+    public GameObject Get(string name)
     {
+        /*
         GameObject obj = null;
         for (int i = 0; i < poolSize; i++)
         {
@@ -30,7 +53,24 @@ public class ObjectPool : MonoBehaviour
                 break;
             }
         }
+        */
+        
+        if(!poolDict.ContainsKey(name))
+        {
+            return null;
+        }
+        GameObject obj = null;
+        for(int i = 0; i< poolDict[name].poolSize; i++)
+        {
+            if (!poolDict[name].ObjsInPool[i])
+            {
+                poolDict[name].ObjsInPool[i].SetActive(true);
+                obj = poolDict[name].ObjsInPool[i];
+                break;
+            }
+        }
         return obj;
+        
     }
 
     public void Release(GameObject obj)
